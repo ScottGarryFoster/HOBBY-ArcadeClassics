99e7075a063b640056a36aec99bf961d4414bc1c
Scott Foster <scottgarryfoster@googlemail.com>
2023-06-25 14:25:43 +0100

    Moved Logic for Player into Snake Player

--- ArcadeRecreations/Assets/Scripts/FQ.GameplayElements/Actors/Player/InteractableActor.cs
+++ ArcadeRecreations/Assets/Scripts/FQ.GameplayElements/Actors/Player/InteractableActor.cs
@@ -20,82 +20,46 @@ namespace FQ.GameplayElements
         /// <summary>
         /// How fast the actor moves each time the actor moves.
         /// </summary>
+        /// <remarks>Internal for testing purposes. </remarks>
         [SerializeField]
         internal float movementSpeed = 0.2f;
 
-        private Vector2 originalPosition;
-        private Vector2Int gridPosition;
-        private float deltaDelay;
-        private Direction currentDirection;
-        private bool receivedInput;
-        
         /// <summary>
         /// Interaction with Gameplay Inputs.
         /// </summary>
         /// <remarks>Injected. </remarks>
         internal IGameplayInputs gameplayInputs;
-
+        
         /// <summary>
-        /// Logic for an actor which moves.
+        /// Start called by Unity.
         /// </summary>
-        private IMovingActor movingActor;
-
-        /// <summary>
-        /// Handles direction inputs from the input IO.
-        /// </summary>
-        private IDirectionInput directionInput;
-
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-            this.originalPosition = this.transform.position;
-            this.gridPosition = new Vector2Int(0, 0);
-
-            this.currentDirection = Direction.Down;
-            this.receivedInput = false;
-
-            this.movingActor = new MovingActor();
-            this.movingActor.Setup(this.transform, movement: 1);
-
-            this.directionInput = new DirectionPressedOrDownInput();
-            this.directionInput.Setup(this.gameplayInputs);
+            ProtectedStart();
+        }
+        
+        /// <summary>
+        /// Overriden by other players.
+        /// </summary>
+        protected virtual void ProtectedStart()
+        {
+            
+        }
+        
+        /// <summary>
+        /// Called every frame update.
+        /// </summary>
+        private void Update()
+        {
+            ProtectedUpdate();
         }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Overriden by over players. Called every frame.
+        /// </summary>
+        protected virtual void ProtectedUpdate()
         {
-            if ((!this.receivedInput || this.currentDirection != Direction.Up) && this.directionInput.PressingInputInDirection(Direction.Down))
-            {
-                this.currentDirection = Direction.Down;
-                this.receivedInput = true;
-            }
-            else if ((!this.receivedInput || this.currentDirection != Direction.Down) && this.directionInput.PressingInputInDirection(Direction.Up))
-            {
-                this.currentDirection = Direction.Up;
-                this.receivedInput = true;
-            }
-            else if (this.currentDirection != Direction.Right && this.directionInput.PressingInputInDirection(Direction.Left))
-            {
-                this.currentDirection = Direction.Left;
-                this.receivedInput = true;
-            }
-            else if (this.currentDirection != Direction.Left && this.directionInput.PressingInputInDirection(Direction.Right))
-            {
-                this.currentDirection = Direction.Right;
-                this.receivedInput = true;
-            }
             
-            
-            this.deltaDelay += Time.deltaTime;
-            if (this.deltaDelay >= MovementSpeed)
-            {
-                this.deltaDelay -= MovementSpeed;
-
-                if (this.receivedInput)
-                {
-                    this.movingActor.MoveActor(this.currentDirection);
-                }
-            }
         }
     }
 
---
+++ ArcadeRecreations/Assets/Scripts/FQ.GameplayElements/Actors/Player/SnakePlayer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace FQ.GameplayElements
+{
+    /// <summary>
+    /// Represents a player in the arcade game snake.
+    /// </summary>
+    public class SnakePlayer : InteractableActor
+    {
+        private float deltaDelay;
+        private Direction currentDirection;
+        private bool receivedInput;
+        
+        /// <summary>
+        /// Logic for an actor which moves.
+        /// </summary>
+        private IMovingActor movingActor;
+
+        /// <summary>
+        /// Handles direction inputs from the input IO.
+        /// </summary>
+        private IDirectionInput directionInput;
+
+        
+        protected override void ProtectedStart()
+        {
+            this.currentDirection = Direction.Down;
+            this.receivedInput = false;
+
+            this.movingActor = new MovingActor();
+            this.movingActor.Setup(this.transform, movement: 1);
+
+            this.directionInput = new DirectionPressedOrDownInput();
+            this.directionInput.Setup(this.gameplayInputs);
+        }
+        
+        protected override void ProtectedUpdate()
+        {
+            if ((!this.receivedInput || this.currentDirection != Direction.Up) && this.directionInput.PressingInputInDirection(Direction.Down))
+            {
+                this.currentDirection = Direction.Down;
+                this.receivedInput = true;
+            }
+            else if ((!this.receivedInput || this.currentDirection != Direction.Down) && this.directionInput.PressingInputInDirection(Direction.Up))
+            {
+                this.currentDirection = Direction.Up;
+                this.receivedInput = true;
+            }
+            else if (this.currentDirection != Direction.Right && this.directionInput.PressingInputInDirection(Direction.Left))
+            {
+                this.currentDirection = Direction.Left;
+                this.receivedInput = true;
+            }
+            else if (this.currentDirection != Direction.Left && this.directionInput.PressingInputInDirection(Direction.Right))
+            {
+                this.currentDirection = Direction.Right;
+                this.receivedInput = true;
+            }
+            
+            this.deltaDelay += Time.deltaTime;
+            if (this.deltaDelay >= MovementSpeed)
+            {
+                this.deltaDelay -= MovementSpeed;
+
+                if (this.receivedInput)
+                {
+                    this.movingActor.MoveActor(this.currentDirection);
+                }
+            }
+        }
+    }
+}
